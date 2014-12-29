755390c761a26d1a459e7780a7a20a7254903dfd
Amit <amit.kumar.agarwal16@gmail.com>
2014-12-29 14:22:00 +0530

    Create PyPyramid.py
    Pyramid made using Python Looping, Its a very immature procedure, just did it for fun.

---
+++ PyPyramid.py
@@ -0,0 +1,11 @@
+__author__ = 'Amit'
+
+# Pyramid made using Python Looping, Its a very immature procedure, just did it for fun.
+
+for i in range(0,30):
+    c = 30 - i
+    print ' ' * c, '-' * i, '-' * i
+
+for j in range(30,0,-1):
+    d = 30 - j
+    print ' ' * d,'-' * j, '-' * j
