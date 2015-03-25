e1548a26d521c2063e77c81a6344bdc61c10cb40
Amit <amit.kumar.agarwal16@gmail.com>
2015-03-25 04:56:18 -0400

    Create Celsius_to_F.py
    A Simple Fahrenheit to Celsius Convertor

---
+++ Celsius_to_F.py
@@ -0,0 +1,11 @@
+#A Simple Fahrenheit to Celsius Convertor
+
+
+def cel_to_fer():
+    F = input("Enter Temperature in Fahrenheit : ")
+    C = (F - 32) * 5/9
+    
+    print "Temperature in Celsius : ", C 
+    
+    
+cel_to_fer()
