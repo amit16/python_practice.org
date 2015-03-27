236c45497359bdbfe0127a3484eecb137819180f
Amit <amit.kumar.agarwal16@gmail.com>
2015-03-27 09:52:35 +0530

    Update greedy_coin_exchanger.py

--- greedy_coin_exchanger.py
+++ greedy_coin_exchanger.py
@@ -2,22 +2,22 @@ __author__ = 'Amit'
 
 amount = input("Enter the amount in dollar")
 
-'''
-if isinstance(amount, float):
-    amount = amount * 100
-'''
-
+amount_cents = int(amount*100)
 
 ans = []
 
-denoms = [25, 16, 5, 1]
+denoms = [25, 10, 5, 1]
 
-while amount > 0:
-    if amount >= denoms[0]:
-        num = amount // denoms[0]
-        amount -= (num * denoms[0])
+while amount_cents > 0:
+    if amount_cents >= denoms[0]:
+        num = amount_cents // denoms[0]
+        amount_cents -= (num * denoms[0])
         ans.append(num)
 
     denoms = denoms[1:]
 
-print "You will get ", sum(ans), " no. of coins as change "
+print "You will get :\n"
+    #, sum(ans), " no. of coins as change "
+
+
+print ans[0], " No. of Quarters\n", ans[1], " No. of Dimes\n", ans[2], " No. of Nickels\n", ans[3], " No. of Pennies\n"
