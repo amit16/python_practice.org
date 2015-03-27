77de80480b78808d05c1bc77edbf2f2331293d4a
Amit <amit.kumar.agarwal16@gmail.com>
2015-03-27 08:36:00 -0400

    Update greedy_coin_exchanger.py

--- greedy_coin_exchanger.py
+++ greedy_coin_exchanger.py
@@ -1,16 +1,15 @@
+#!/usr/bin/python
+
 __author__ = 'Amit'
 
-'''
-Its a coin exchanger, it asks the user for an amount in dollars,
-then calculates the least number of coins required in exchange of that
-"Uses Greedy Algorithm"
-'''
+while True:
+    amount = input("Enter the amount(in dollar), u need change for : ")
+    if amount > 0:
+        break
 
-amount = input("Enter the amount(in dollar), u need change for : ")
 amount_cents = int(amount*100)
 ans = []
 
-#The value of denom can be changed as per your requirement for coins to be used
 denoms = [25, 10, 5, 1]
 
 while amount_cents > 0:
@@ -38,3 +37,9 @@ print "You will get :"
 for i in range(0, length):
     print ans[i]," ", change_values[i]
     i =-1
+    
+   
+    
+   
+    
+
