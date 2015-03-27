71af9c832fc66c2f4388d8787b28b46562bb62f8
Amit <amit.kumar.agarwal16@gmail.com>
2015-03-27 07:33:57 -0400

    Update greedy_coin_exchanger.py
    Its a coin exchanger, it asks the user for an amount in dollars,
    then calculates the least number of coins required in exchange of that
    "Uses Greedy Algorithm"

--- greedy_coin_exchanger.py
+++ greedy_coin_exchanger.py
@@ -1,9 +1,13 @@
 __author__ = 'Amit'
 
-amount = input("Enter the amount in dollar")
+'''
+Its a coin exchanger, it asks the user for an amount in dollars,
+then calculates the least number of coins required in exchange of that
+"Uses Greedy Algorithm"
+'''
 
+amount = input("Enter the amount(in dollar), u need change for : ")
 amount_cents = int(amount*100)
-
 ans = []
 
 denoms = [25, 10, 5, 1]
@@ -16,8 +20,20 @@ while amount_cents > 0:
 
     denoms = denoms[1:]
 
-print "You will get :\n"
-    #, sum(ans), " no. of coins as change "
+    
+length = len(ans)
 
+change_values = {
 
-print ans[0], " No. of Quarters\n", ans[1], " No. of Dimes\n", ans[2], " No. of Nickels\n", ans[3], " No. of Pennies\n"
+    0:'- Quarters',
+    1:'- Dimes',
+    2:'- Nickel',
+    3:'- Penies',
+     
+}
+
+print "You will get :"
+
+for i in range(0, length):
+    print ans[i]," ", change_values[i]
+    i =-1
