90138b995556121c95419968eccb12dc625f647f
Amit <amit.kumar.agarwal16@gmail.com>
2015-05-05 17:39:37 +0530

    Update Validate_PhoneNum.py

--- Validate_PhoneNum.py
+++ Validate_PhoneNum.py
@@ -9,6 +9,8 @@ The first line contains an integer N followed by N lines, each containing some s
 Output Format
 
 For every string listed, print YES if it is a valid mobile number and NO if it isn’t.
+
+Note :- Phone Number shouldn't contain any characters
 '''
 
 import re
@@ -23,7 +25,7 @@ for i in range(0, num):
 for j in range(0, num):
     l = len(digit_list[j])
     contains_789 = re.search(r'^[7,8,9]',digit_list[j]) #Checks whther String starts with 7, 8 or 9 
-    contains_alpha = bool(re.search(r'[A-Z]', digit_list[j])) #Checks whther String contains A-Z chars or not
+    contains_alpha = bool(re.search(r'\w', digit_list[j])) #Checks whther String contains any chars or not
     print contains_alpha
     if  l == 10 and contains_789 and (contains_alpha) == False :
         print "YES"
