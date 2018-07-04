3cfd46d3518a456e5670da2b48cdd3825c497433
Amit <amit.kumar.agarwal16@gmail.com>
2018-07-04 10:55:45 +0530

    Create switch_as_dict.py

---
+++ switch_as_dict.py
@@ -0,0 +1,11 @@
+def dispatch_dict(operator, x, y):
+    return {
+        'add': lambda: x + y,
+        'sub': lambda: x - y,
+        'mul': lambda: x * y,
+        'div': lambda: x / y,
+    }.get(operator, lambda: None)()
+
+
+
+print dispatch_dict('mul', 2, 3)
